10a2aa9b0b734e95cb59b16fb18ad20419682008
Samet-Berkay-Taskin <berkaytaskin99@hotmail.com>
2022-07-30 18:15:10 +0300

    Files upload

---
+++ kedi-kopek1/kedi-1.py
@@ -0,0 +1,68 @@
+from keras.models import load_model
+import numpy as np
+import cv2
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import tensorflow as tf
+from fastapi.responses import JSONResponse
+import keras
+from keras.preprocessing import image
+
+
+new_model = load_model('kedi-kopek.h5')
+new_model.summary()
+
+
+app = FastAPI()
+
+
+
+"""test_foto1 = keras.utils.load_img('dog3.jpg', target_size=(64,64))
+test_foto1 = tf.keras.utils.img_to_array(test_foto1)
+test_foto1 = test_foto1/255
+test_foto1 = np.expand_dims(test_foto1, axis=0)
+sonuc1 = new_model.predict(test_foto1)
+if sonuc1[0][0] > 0.5:
+    Prediction = 'Dog'
+else:
+    Prediction = 'Cat'
+print(Prediction)"""
+
+
+
+
+
+@app.get("/")
+async def root():
+    return {"Resimdeki kedi mi köpek mi ?"}
+
+
+@app.post("/predict/")
+async def predict_image(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        image = np.frombuffer(contents, np.uint8)
+        print("SIIZEEE2", image.shape)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        print("SIIZEEE2", image.shape)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        print("SIIZEEE2", image.shape)
+        image_arr = np.array(image, dtype=np.float32)
+        print("SIIZEEE2", image_arr.shape)
+        resized = cv2.resize(image_arr, (64, 64), interpolation=cv2.INTER_AREA)
+        print("SIIZEEE2", resized.shape)
+        test_foto1 = np.expand_dims(resized, axis=0)
+        print("SIIZEEE2", test_foto1.shape)
+
+        sonuc1 = new_model.predict(test_foto1)
+
+        if sonuc1[0][0] > 0.5:
+            Prediction = 'Dog'
+        else:
+            Prediction = 'Cat'
+
+        return {Prediction}
+
+    except Exception as e:
+        print(str(e))
+        return JSONResponse(content = {"status": "An error occurred."})
